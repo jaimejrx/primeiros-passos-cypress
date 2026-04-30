65949b12558dfc6dfe02c3fd88e8b45f992d41e8
Jaime Paula Farias Junior <jaime.junior@zoomtecnologia.com.br>
2026-04-30 16:47:17 -0300

    User update scenario added

---
+++ cypress/e2e/user.spec.cy.js
@@ -0,0 +1,50 @@
+import userData from '../fixtures/users/userData.json'
+
+describe('Orange HRM Tests', () => {
+
+  const selectorsList = {
+    usernameField: '[name="username"]',
+    passwordField: '[name="password"]',
+    loginButton: '[type="submit"]',
+    sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
+    dashboardGrid: '.orangehrm-dashboard-grid',
+    wrongCredentialAlert: '[role="alert"]',
+    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameField: '[name="firstName"]',
+    lastNameField: '[name="lastName"]',
+    genericField: '.oxd-input--active',
+    dateField: '[placeholder="yyyy-dd-mm"]',
+    dateCloseButton: ".--close",
+    submitButton: '[type="submit"]'
+   }  
+   
+  it.only('User Info Update - Success', () => {
+    cy.visit('/auth/login')
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
+    cy.get(selectorsList.loginButton).click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
+    cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
+    // cy.get(selectorsList.genericField).eq(3).clear().type('NicknameTest') - campo excluido do site orangehrm
+    cy.get(selectorsList.genericField).eq(3).clear().type('EmployeeId')
+    cy.get(selectorsList.genericField).eq(4).clear().type('OtherIdTest')
+    cy.get(selectorsList.genericField).eq(5).clear().type('DriversLicenseTest')
+    cy.get(selectorsList.genericField).eq(6).clear().type('2026-30-04')
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get('body').should('contain', 'Successfully Updated')
+    cy.get('.oxd-toast-close')
+    // cy.get(selectorsList.genericField).eq(8).clear().type('ssnNumberTest') - campo excluido do site orangehrm
+    // cy.get(selectorsList.genericField).eq(9).clear().type('sinNumberTest') - campo excluido do site orangehrm
+  })
+  it('Login - Fail', () => {
+    cy.visit('/auth/login')
+    cy.get(selectorsList.usernameField).type('userData.userFail.username')
+    cy.get(selectorsList.passwordField).type('userData.userFail.password')
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.wrongCredentialAlert)
+  })  
+})
