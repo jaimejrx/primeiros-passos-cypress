43344d1bba6bea26da657a8fa5da672add88e63e
Jaime Paula Farias Junior <jaime.junior@zoomtecnologia.com.br>
2026-05-05 12:11:08 -0300

    Add page objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,37 +1,40 @@
 import userData from '../fixtures/users/userData.json'
+import LoginPage from '../pages/loginPage'
+import DashboardPage from '../pages/dashboardPage'
+import MenuPage from '../pages/menuPage'
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPage = new MenuPage()
 
 describe('Orange HRM Tests', () => {
 
   const selectorsList = {
-    usernameField: '[name="username"]',
-    passwordField: '[name="password"]',
-    loginButton: '[type="submit"]',
-    sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
-    dashboardGrid: '.orangehrm-dashboard-grid',
-    wrongCredentialAlert: '[role="alert"]',
-    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
-    firstNameField: '[name="firstName"]',
-    lastNameField: '[name="lastName"]',
-    genericField: '.oxd-input--active',
-    dateField: '[placeholder="yyyy-dd-mm"]',
-    genericCombobox: '.oxd-select-text--arrow',
-    secondItemCombobox: '.oxd-select-dropdown > :nth-child(3)',
-    thirdItemCombobox: '.--selected',
-    dateCloseButton: '.--close',
-    submitButton: '[type="submit"]',
+
+    
+    firstNameField: "[name='firstName']",
+    lastNameField: "[name='lastName']",
+    genericField: ".oxd-input--active",
+    dateField: "[placeholder='yyyy-dd-mm']",
+    genericCombobox: ".oxd-select-text--arrow",
+    secondItemCombobox: ".oxd-select-dropdown > :nth-child(3)",
+    thirdItemCombobox: ".--selected",
+    dateCloseButton: ".--close",
+    submitButton: "[type='submit']",
+
    }  
    
   it.only('User Info Update - Success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.dashboardGrid)
-    cy.get(selectorsList.myInfoButton).click()
+    loginPage.accessLoginPage()
+    loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
+
+    dashboardPage.checkDashboardPage()
+
+    menuPage.accessMyInfo()
+
     cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
     cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
-    // cy.get(selectorsList.genericField).eq(3).clear().type('NicknameTest') - campo excluido do site orangehrm
+    // // cy.get(selectorsList.genericField).eq(3).clear().type('NicknameTest') - campo excluido do site orangehrm
     cy.get(selectorsList.genericField).eq(3).clear().type('EmployeeId')
     cy.get(selectorsList.genericField).eq(4).clear().type('OtherIdTest')
     cy.get(selectorsList.genericField).eq(5).clear().type('DriversLicenseTest')
